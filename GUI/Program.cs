595a23270413b9c628671de48845486dac19808b
Pham Ngan <tuyettngann515@gmail.com>
2026-03-23 18:04:57 +0700

    ko dùng

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -11,13 +11,13 @@
             ApplicationConfiguration.Initialize();
 
             // 1. Mở bảng đăng nhập lên trước
-            frmLogin login = new frmLogin();
+            frmPhieuThu login = new frmPhieuThu();
 
             // 2. Nếu bấm Đăng nhập thành công (OK) thì mới mở tiếp
             if (login.ShowDialog() == DialogResult.OK)
             {
                 // Mượn tạm frmBase làm giao diện chính cho đơn giản
-                Application.Run(new frmMain());
+                Application.Run(new frmPhieuThu());
             }
         }
     }
